3ac1e38aeec25756ee2b21795c8304cfe350e07f
istekhar <istekhararkam25@gmail.com>
2024-10-01 19:34:54 +0530

    cart card completed

--- src/components/cardsData.js
+++ src/components/cardsData.js
@@ -1,7 +1,7 @@
 const cardsData = [
   {
     id: 1,
-    rname: "None Veg Thali",
+    rname: "Non Veg Thali",
     imgdata:
       "https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp",
     address: "North Indian, Biryani, Mughlai",
